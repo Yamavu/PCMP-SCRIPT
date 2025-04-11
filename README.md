a149e193885ec62013a0138f94926151efccea83
seanbutler <butlersean@gmail.com>
2025-04-11 23:16:03 +0100

    working script and basic instructions

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# PCMP-Cmake
-PicoCalcMicroPython CMake Scripts
+# PCMP-script
+
+an MIT licensed bash script to automatically download everything ncessary to get micropython running on your picocalc. pretty simple, but less error prone than manually doing the steps.
+
+works on ubuntu with python and git installed, should work from windows WSL, let me know if it works for you.
+
+currently paraeter is set for the included RPI PICO board. if you have upgraded to a RPI2 or RPI2W, edit line 36 from RPI_PICO to RPI_PICO2 or RPI_PICO2_W
+ 
+## Steps
+
+1. clone this repo somewhere
+
+git clone <this repos url>
+
+2. Inside the folder...
+
+~~~
+cd build
+../scipts/runme.sh
+make
+~~~
+
+3. Use BOOTSEL and Thonny to move the files over to your PicoCalc in the usual way. Not managed to automate this step yet.
+
---
+++ scripts/runme.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+project_dir=`pwd`;
+
+# echo "Project Dir = $project_dir"
+
+if [ -d PicoCalc-micropython-driver ]; then
+    rm -rf PicoCalc-micropython-driver
+fi
+
+git clone https://github.com/zenodante/PicoCalc-micropython-driver
+
+
+if [ -d micropython ]; then
+    rm -rf micropython
+fi
+
+git clone https://github.com/micropython/micropython
+
+cp $project_dir/PicoCalc-micropython-driver/pico_files/fbconsole.py ./micropython/ports/rp2/modules
+cp $project_dir/PicoCalc-micropython-driver/pico_files/picocalc.py ./micropython/ports/rp2/modules
+
+cd $project_dir
+cd micropython/ports/rp2
+git submodule update --init --recursive
+
+
+if [ -d build ]; then
+    rm -rf build
+fi
+
+mkdir build && cd build
+
+cmake .. \
+    -DUSER_C_MODULES="$project_dir/PicoCalc-micropython-driver/micropython.cmake" \
+    -DMICROPY_BOARD=RPI_PICO
+
+#    RPI_PICO
+#    RPI_PICO2
+#    RPI_PICO2_W
+
+make
+
+bash <(wget -O - https://thonny.org/installer-for-linux )
